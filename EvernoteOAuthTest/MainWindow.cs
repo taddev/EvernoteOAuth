dfa3304749f70da6aa1e509d7af50c5b5f5c090d
Tad DeVries <tad@splunk.net>
2012-11-19 19:17:28 -0700

    Added OAuth library and started some comments

--- EvernoteOAuthTest/MainWindow.cs
+++ EvernoteOAuthTest/MainWindow.cs
@@ -2,25 +2,58 @@ using System;
 using Gtk;
 using GtkSharp;
 
+/// <summary>
+/// Main window.
+/// </summary>
 public partial class MainWindow: Gtk.Window
 {	
+	/// <summary>
+	/// Initializes a new instance of the <see cref="MainWindow"/> class.
+	/// </summary>
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
 		Console.WriteLine( "Window Built\n" );
 	}
-	
+
+	/// <summary>
+	/// Raises the delete event event.
+	/// </summary>
+	/// <param name='sender'>
+	/// Sender.
+	/// </param>
+	/// <param name='a'>
+	/// The alpha component.
+	/// </param>
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
 	{
 		Application.Quit ();
 		a.RetVal = true;
 	}
-	
+
+	/// <summary>
+	/// Raises the file menu quit actived event.
+	/// </summary>
+	/// <param name='sender'>
+	/// Sender.
+	/// </param>
+	/// <param name='e'>
+	/// E.
+	/// </param>
 	protected virtual void OnFileMenuQuitActived (object sender, System.EventArgs e)
 	{
 		Application.Quit ();
 	}
 
+	/// <summary>
+	/// Raises the login button clicked event.
+	/// </summary>
+	/// <param name='sender'>
+	/// Sender.
+	/// </param>
+	/// <param name='e'>
+	/// E.
+	/// </param>
 	protected void OnLoginButtonClicked (object sender, EventArgs e)
 	{
 		MessageDialog md = new MessageDialog (this, 
